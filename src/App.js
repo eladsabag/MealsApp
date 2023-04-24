7cf702f4a5bbb80e6391fbe23fc5fad2142fe3e9
eladsabag2 <eladsabag2@gmail.com>
2023-04-24 13:01:56 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {StatusBar, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <>
+      <StatusBar backgroundColor={'#351401'} />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {backgroundColor: '#351401'},
+            headerTintColor: 'white',
+            contentStyle: {backgroundColor: '#2f2f25'},
+          }}>
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: 'All Categories',
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            // options={({route, navigation}) => {
+            //   const categoryId = route.params.categoryId;
+            //   return {
+            //     title: categoryId
+            //   };
+            // }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#241900',
+  },
+});
+
+export default App;
